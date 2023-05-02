f69d6264491e684d9cceb669f62416dcf5839ec3
Bruno Wu <brunogamawu@gmail.com>
2023-05-01 22:52:06 -0300

    first commit

---
+++ src/test/java/com/pismo/codechallenger/CodechallengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pismo.codechallenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodechallengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
