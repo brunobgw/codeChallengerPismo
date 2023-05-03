18e176b612680f133500515b4f7728ff6c3631c5
Bruno Wu <brunogamawu@gmail.com>
2023-05-03 14:53:26 -0300

    add constraints fk database

--- src/main/java/com/pismo/codechallenger/CodechallengerApplication.java
+++ src/main/java/com/pismo/codechallenger/CodechallengerApplication.java
@@ -2,8 +2,6 @@ package com.pismo.codechallenger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class CodechallengerApplication {
--- src/main/resources/db/migration/V3.0__create_transactions.sql
+++ src/main/resources/db/migration/V3.0__create_transactions.sql
@@ -3,5 +3,7 @@ CREATE TABLE transactions (
 	account_id int NOT NULL,
 	operation_type_id int NOT NULL,
 	amount double NOT NULL,
-	event_date datetime NOT NULL
+	event_date datetime NOT NULL,
+    FOREIGN KEY (account_id) REFERENCES account(id),
+    FOREIGN KEY (operation_type_id) REFERENCES operation_types(id)
 );
