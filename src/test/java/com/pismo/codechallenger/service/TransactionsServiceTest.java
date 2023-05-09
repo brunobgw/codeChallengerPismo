fed7d4b94bfa0123cd442302b49ad766164f75a7
Bruno Wu <brunogamawu@gmail.com>
2023-05-09 10:38:26 -0300

    flyway mysql

---
+++ src/test/java/com/pismo/codechallenger/service/TransactionsServiceTest.java
@@ -0,0 +1,12 @@
+package com.pismo.codechallenger.service;
+
+import org.junit.jupiter.api.Test;
+
+class TransactionsServiceTest {
+
+	@Test
+	void test() {
+		//fail("Not yet implemented");
+	}
+
+}
